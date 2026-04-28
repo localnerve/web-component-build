a6334d0c21e1a701dcd8fce25328beb3657ce7f3
localnerve <alex@localnerve.com>
2026-04-28 13:49:53 -0400

    @2.0.0 - node 20+, devdeps updates

--- eslint.config.js
+++ eslint.config.js
@@ -1,43 +1,66 @@
 import js from '@eslint/js';
-import jest from 'eslint-plugin-jest';
 import globals from 'globals';
+import nodePlugin from 'eslint-plugin-n';
 
-export default [
-  {
-    files: [
-      '__tests__/fixtures/**/*'
-    ],
-    languageOptions: {
-      globals: {
-        ...globals.browser
-      }
-    }
-  },
-  {
-    files: [
-      '__tests__/**/*.spec.js'
-    ],
-    ...jest.configs['flat/recommended'],
-    languageOptions: {
-      globals: {
-        ...jest.configs['flat/recommended'].languageOptions.globals,
-        ...globals.node
-      }
-    },
-    rules: {
-      ...jest.configs['flat/recommended'].rules,
-      'jest/no-conditional-expect': 'off'
-    }
-  },
-  js.configs.recommended,
-  {
-    rules: {
-      indent: [2, 2, {
-        SwitchCase: 1,
-        MemberExpression: 1
-      }],
-      quotes: [2, 'single'],
-      'dot-notation': [2, {allowKeywords: true}]
+const nodeRules = nodePlugin.configs['flat/recommended'].rules;
+const commonRules = {
+  indent: [2, 2, {
+    SwitchCase: 1,
+    MemberExpression: 1
+  }],
+  quotes: [2, 'single'],
+  'dot-notation': [2, {allowKeywords: true}]
+};
+
+export default [{
+  name: 'global',
+  ignores: [
+    'coverage/**'
+  ]
+}, {
+  name: 'test-fixtures',
+  files: [
+    '__tests__/fixtures/**/*'
+  ],
+  languageOptions: {
+    globals: {
+      ...globals.browser
     }
   }
-];
+}, {
+  name: 'test',
+  plugins: { n: nodePlugin },
+  files: [
+    '__tests__/**/*.spec.js'
+  ],
+  languageOptions: {
+    globals: {
+      ...globals.node
+    }
+  },
+  rules: {
+    ...js.configs.recommended.rules,
+    ...nodeRules,
+    ...commonRules,
+    'n/no-unsupported-features/node-builtins': ['error', {
+      'ignores': ['import.meta.dirname', 'test.describe'],
+      'allowExperimental': true
+    }],
+  }
+}, {
+  name: 'lib',
+  plugins: { n: nodePlugin },
+  files: [
+    'lib/**'
+  ],
+  languageOptions: {
+    globals: {
+      ...globals.node
+    }
+  },
+  rules: {
+    ...js.configs.recommended.rules,
+    ...nodeRules,
+    ...commonRules
+  }
+}];
